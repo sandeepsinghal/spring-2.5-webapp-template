312c465672a12c8e31266eaada1f44c279487810
Sandeep Singhal <sandeepsinghal@gmail.com>
2015-04-22 11:59:14 +0530

    basic code for hello world

---
+++ src/main/java/com/sandeepsinghal/spring/sample/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.sandeepsinghal.spring.sample;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ssinghal on 4/22/15.
+ */
+public class HelloWorldController implements Controller
+{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		return new
+	}
+}
